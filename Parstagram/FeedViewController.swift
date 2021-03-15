cc47b7dd791283780aa617c7548d16e60477118e
par5ul1 <elexir0@gmail.com>
2021-03-14 23:21:08 -0700

    Done

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Parse
-import Alamofire
+import AlamofireImage
 
 class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -26,7 +26,7 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        var query = PFQuery(className: "Posts")
+        let query = PFQuery(className: "Posts")
         query.includeKey("author")
         query.limit = 20
         
@@ -39,21 +39,24 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
-        let post = posts[indexPath.row]
+        let post = posts[posts.count - indexPath.row - 1]
         
         let user = post["author"] as! PFUser
         cell.usernameLabel.text = user.username
         
-        cell.captionLabel.text = post["caption"] as! String
+        cell.captionLabel.text = post["caption"] as? String
         
         let imageFile = post["image"] as! PFFileObject
         let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
         
         return cell
     }
